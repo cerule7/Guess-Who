756935ded40be423dbdf15f5da2b4b9bf48fa8d6
Charles Rule <cruleis@gmail.com>
2018-06-12 14:43:02 -0500

    character object

---
+++ character.py
@@ -0,0 +1,24 @@
+class Character:
+	
+	name = ''
+	female = False
+	hat = False
+	glasses = False
+	beard = False
+	mustache = False
+	cheeks = False
+	smile = False
+	hairColor = '' 
+
+	def __init__(self, name, female, hat, glasses, beard, mustache, cheeks, smile, hairColor):
+		self.name = name
+		self.female = female
+		self.hat = hat
+		self.glasses = glasses
+		self.beard = beard
+		self.mustache = mustache
+		self.cheeks = cheeks
+		self.smile = smile
+		self.hairColor = hairColor
+
+
