1ddd2c9dc0dfda3d3d134b3b025395d1b89cb233
William Fallin <32699361+RogaineFoam@users.noreply.github.com>
2018-06-18 11:26:58 -0500

    Finished random agent

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
-import player.py
+from player import Player
+from numpy import random
 
-class Agent(player.Player):
+class Agent(Player):
 
 	gameboard = ''
 	score = 0 
@@ -10,6 +11,33 @@ class Agent(player.Player):
 	def __init__(self, gameboard, selectedCharacter):
 		self.gameboard = gameboard
 		self.selectedCharacter = selectedCharacter
+	
+	# Makes a random choice
+	# 24 < choice > -1 for random character
+	# 40 < choice > 23 for random trait
+	def guessRandom(self):
+		choice = -1
+		
+		action = self.chooseRandom()
+		
+		if action == "trait":
+			choice = random.randint(24, 40)
+		else:
+			choice = random.randint(0, 24)
+		
+		return choice
+	
+	# Makes a random number to make a binary choice 
+	def chooseRandom(self):
+		action = ""
+		
+		if random.randint(0, 2) == 1:
+			action = "trait"
+		else:
+			action = "character"
+		
+		return action
+
 
 #===============================================================================
 #	Unmodified methods
