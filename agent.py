3a63c01791d0214489ca4b515e203611f3978010
William Fallin <32699361+RogaineFoam@users.noreply.github.com>
2018-06-18 10:17:41 -0500

    have to commit this because i need to get an update

--- agent.py
+++ agent.py
@@ -11,23 +11,27 @@ class Agent(player.Player):
 		self.gameboard = gameboard
 		self.selectedCharacter = selectedCharacter
 
-	def getName(self):
-		return self.name
+#===============================================================================
+#	Unmodified methods
 
-	def getSelected(self):
-		return self.selectedCharacter
-
-	def getBoard(self):
-		return self.gameboard
-
-	def setBoard(self, board):
-		self.gameboard = board
-
-	def setScore(self, score):
-		self.score = score
-
-	def getScore(self):
-		return self.score
-
-	def setSelected(num):
-		selectedCharacter = num 
+# 	def getName(self):
+# 		return self.name
+# 
+# 	def getSelected(self):
+# 		return self.selectedCharacter
+# 
+# 	def getBoard(self):
+# 		return self.gameboard
+# 
+# 	def setBoard(self, board):
+# 		self.gameboard = board
+# 
+# 	def setScore(self, score):
+# 		self.score = score
+# 
+# 	def getScore(self):
+# 		return self.score
+# 
+# 	def setSelected(num):
+# 		selectedCharacter = num 
+#===============================================================================
