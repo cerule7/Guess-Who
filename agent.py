aaa6de537c914acc16a73b0c969a4bcd5404764f
William Fallin <32699361+RogaineFoam@users.noreply.github.com>
2018-06-18 10:12:33 -0500

    adding a random agent

---
+++ agent.py
@@ -0,0 +1,33 @@
+import player.py
+
+class Agent(player.Player):
+
+	gameboard = ''
+	score = 0 
+	name = 'Skynet'
+	selectedCharacter = 0
+
+	def __init__(self, gameboard, selectedCharacter):
+		self.gameboard = gameboard
+		self.selectedCharacter = selectedCharacter
+
+	def getName(self):
+		return self.name
+
+	def getSelected(self):
+		return self.selectedCharacter
+
+	def getBoard(self):
+		return self.gameboard
+
+	def setBoard(self, board):
+		self.gameboard = board
+
+	def setScore(self, score):
+		self.score = score
+
+	def getScore(self):
+		return self.score
+
+	def setSelected(num):
+		selectedCharacter = num 
