9615669a62724846bc09d8b28f7142f5d694e941
Charles Rule <cruleis@gmail.com>
2018-06-20 17:16:05 -0500

    changed numflip method + removed redundant actions

--- game.py
+++ game.py
@@ -8,6 +8,8 @@ class Game:
 	p1 = None
 	p2 = None
 	numTurns = 0
+	state = ''
+	numFlipped = 0
 
 	def __init__(self):
 		sel = np.random.randint(0, 24)
@@ -21,6 +23,15 @@ class Game:
 		self.p2 = Agent("Alex", g2, j)
 		print(self.p2.getName() + " selected " + self.p2.getBoard().getCharacter(self.p2.getBoard().getSelected()).getName())
 		numTurns = 0 
+		self.state = 'START'
+		self.numFlipped = 0
+
+	def getState(self):
+		if(self.state == 'WIN' or self.state == 'LOST'):
+			return state
+		else:
+			return str(self.numFlipped)
+
 
 	def getAction(self, i):
 		if self.numTurns % 2 == 0:
@@ -38,43 +49,53 @@ class Game:
 			if(i == otherplayer.getBoard().getSelected()):
 				print("CORRECT GUESS")
 				player.setScore(player.getScore() + 1)
+				if numTurns % 2 == 0:
+					self.state = 'WON'
+				else:
+					self.state = 'LOST'
 			else:
 				print("INCORRECT GUESS")
 				otherplayer.setScore(otherplayer.getScore() + 1)
+				if numTurns % 2 == 0:
+					self.state = 'LOST'
+				else:
+					self.state = 'WON'
 			return 
 		#y/n questions
+		self.numFlipped = 0
 		if i == 24:
-			player.getBoard().updateList(player.getBoard().askQ('isFemale', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('isFemale', otherplayer.getBoard())
+			player.getBoard().updateList(characterList
 		if i == 25:
-			player.getBoard().updateList(player.getBoard().askQ('hasHat', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('hasHat', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 26:
-			player.getBoard().updateList(player.getBoard().askQ('hasGlasses', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('hasGlasses', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 27:
-			player.getBoard().updateList(player.getBoard().askQ('hasBeard', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('hasBeard', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 28:
-			player.getBoard().updateList(player.getBoard().askQ('hasMustache', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('hasMustache', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 29:
-			player.getBoard().updateList(player.getBoard().askQ('hasRosyCheeks', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('hasRosyCheeks', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 30:
-			player.getBoard().updateList(player.getBoard().askQ('isSmiling', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('isSmiling', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		if i == 31:
-			player.getBoard().updateList(player.getBoard().askQ('isBald', otherplayer.getBoard()))
-		if i == 32:
-			player.getBoard().updateList(player.getBoard().askQ('hasGlasses', otherplayer.getBoard()))
-		if i == 33:
-			player.getBoard().updateList(player.getBoard().askQ('hasBeard', otherplayer.getBoard()))
-		if i == 34:
-			player.getBoard().updateList(player.getBoard().askQ('hasMustache', otherplayer.getBoard()))
+			characterList, self.numFlipped = player.getBoard().askQ('isBald', otherplayer.getBoard())
+			player.getBoard().updateList(characterList)
 		#binary search
-		if i == 35:
-			binaryPositions, characterlist = player.getBoard().binarySearch(player.getBinaryPositions(), otherplayer.getBoard())
+		if i == 32:
+			binaryPositions, characterlist, self.numFlipped = player.getBoard().binarySearch(player.getBinaryPositions(), otherplayer.getBoard())
 			player.getBoard().updateList(characterlist)
 			player.setBinaryPositions(binaryPositions)
 		#hair colors
 		else:
-			characterlist = player.getBoard().askHairColor(i, otherplayer.getBoard())
+			characterlist, self.numFlipped = player.getBoard().askHairColor(i, otherplayer.getBoard())
 			player.getBoard().updateList(characterlist)
-
 		if self.numTurns % 2 == 0:
 			self.p1 = player
 		else:
@@ -105,6 +126,7 @@ class Game:
 					if(self.p1.getBoard().numberActive() <= 1):
 						self.p1.setScore(self.p1.getScore() + 1)
 						print("PLAYER 1 WINS")
+						self.state = 'WON'
 						gameOver = True
 			else: 
 				p2turns += 1
@@ -124,6 +146,7 @@ class Game:
 					if(self.p2.getBoard().numberActive() <= 1):
 						self.p2.setScore(self.p2.getScore() + 1)
 						print("PLAYER 2 WINS")
+						self.state = 'LOST'
 						gameOver = True
 			self.numTurns += 1
 			print("TOTAL TURNS: " + str(self.numTurns))
